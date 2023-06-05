2b62dda35e4434beebd54e3334c07b107c2658b3
vytre <vegaytre@gmail.com>
2023-06-05 12:11:15 +0200

    Added dev scripts

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import {createRoot} from "react-dom/client"
+import { createRoot } from "react-dom/client";
 
 const element = document.getElementById("app");
 const root = createRoot(element);
 
-root.render(<h1>Hello from React</h1>)
-
+root.render(<h1>Now with dev scripts</h1>);
