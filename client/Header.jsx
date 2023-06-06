c851947eded9a328f71e31ee955a21a1c6719367
CatsEatingCake <jale004@student.kristiania.no>
2023-06-06 13:09:45 +0200

    header 2.0

---
+++ client/Header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+    return (
+        <div className="containerHeader">
+            <div className="header">
+                <h1 className="hjem">Hjem</h1>
+                <h1 className="dilemmaer">Dilemmaer</h1>
+                <h1 className="arkiv">Arkiv</h1>
+            </div>
+            <div className="infoBox">
+            <h1 className="title">Etisk Spill</h1>
+                <div className="infoTextBox">
+                    
+                </div>
+                <div className="startGame">
+                    
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Header;
