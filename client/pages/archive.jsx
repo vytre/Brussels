d97ce8e1054f1fff8b71e7a9941d1f46781c770e
dpoxodp <89521915+dpoxodp@users.noreply.github.com>
2023-06-07 23:26:25 +0200

    wip

--- client/pages/archive.jsx
+++ client/pages/archive.jsx
@@ -28,11 +28,12 @@ export function Archive() {
                   <div className="h4">Arne var en norsk filosof, kjent for sin lidenskap til natur og mennesker. Han blir sett på som mannen bak dypøkologien og var en veldig innflytningsrik filosof. Arne sine ideer om verden er like aktuelle idag som de var når han levde.</div>
               </div>
           </div>
+
           <div className="infoContainer">
               <div className="box">
                   <div className="h2">Arne Næss kunnskapssamling</div>
                   <div className="h4">ANCEP har til nå identifisert i overkant av 3400 dokumenter. En oversikt over disse finner du her. Samlingen inneholder alt fra; bøker, artikler, brev, bilder.</div>
-                  <button className="btn"></button>
+                  <button className="btn">Kunnskapssamlingen</button>
               </div>
               <div className="arneAktivImg"></div>
           </div>
