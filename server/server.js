90987cbe5e0d051527cbc0a8c5481226137ccea0
vytre <vegaytre@gmail.com>
2023-06-07 14:07:49 +0200

    Updateed server.js to handle GET request

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 app.use(express.static("../client/dist"));
+
+app.use((req, res, next) => {
+    if (req.method === "GET") {
+        return res.sendFile(path.resolve("../client/dist/index.html"))
+    } else {
+        next();
+    }
+});
 
 const server = app.listen(process.env.PORT || 3000, ()=> {
     console.log(`Server starting at http://localhost:${server.address().port}`);
