3573fea2dce125e6a5487dc65cddf3497df9bd15
vytre <vegaytre@gmail.com>
2023-06-08 11:39:12 +0200

    Frontpage Broken, but "Dilemma" working

--- client/application.jsx
+++ client/application.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Frontpage } from "./pages/frontpage.jsx";
+import { IntroQuestionsOne } from "./pages/question.jsx";
 
 export function Application() {
   return (
@@ -9,7 +10,7 @@ export function Application() {
         <Routes>
           <Route path={"/"} element={<Frontpage />}></Route>
           <Route path={"/arkiv"} element={<h1>Arkiv Side</h1>}></Route>
-          {/*<Route path={"/dilemma"} element={<IntroQuestionsOne />}></Route>*/}
+          <Route path={"/dilemma"} element={<IntroQuestionsOne />}></Route>
           <Route path={"*"} element={<h1>Page not found</h1>}></Route>
         </Routes>
       </main>
