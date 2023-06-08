117a6c20ce52fe4e017826006bf9b4f4929f4d59
vytre <vegaytre@gmail.com>
2023-06-08 17:09:58 +0200

    Added archive 🟢

--- client/application.jsx
+++ client/application.jsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Frontpage } from "./pages/frontpage.jsx";
 import { DilemmaRoute } from "./pages/dilemmaRoute.jsx";
+import { Archive } from "./pages/archive.jsx";
 
 export function Application() {
   return (
     <>
       <Routes>
         <Route path={"/"} element={<Frontpage />}></Route>
-        <Route path={"/arkiv"} element={<h1>Arkiv Side</h1>}></Route>
+        <Route path={"/archive"} element={<Archive />}></Route>
         {/*<Route path={"/dilemma"} element={<QuestionTest />}></Route>*/}
         <Route path={"/dilemma/*"} element={<DilemmaRoute />}></Route>
         <Route path={"*"} element={<h1>Page not found</h1>}></Route>
---
+++ client/pages/archive.jsx
@@ -0,0 +1,75 @@
+import "../stylesheets/archive.css";
+export function Archive() {
+  return (
+    <div className="page">
+      <div className="topContainer">
+        <div className="containerHeader">
+          <div className="header">
+            <div className="logo"></div>
+            <h1 className="hjem">Hjem</h1>
+            <h1 className="dilemmaer">Dilemmaer</h1>
+            <h1 className="arkiv">Arkiv</h1>
+          </div>
+        </div>
+        <div className="topBox">
+          <div className="topInfoBox">
+            <div className="headingContainer">
+              <div className="heading">Arnes Historie</div>
+            </div>
+            <div className="bgBoxContainer">
+              <div className="bgBox">
+                <div className="text">
+                  Arne Næss var en norsk filosof som var en de mest
+                  innflytelsesrike intellektuelle i Norge i andre halvdel av
+                  1900-tallet. Gjennom Næss' innflytelse fikk analytisk filosofi
+                  en dominerende stilling i Norge i tiårene etter andre
+                  verdenskrig. Han var også kjent som fjellklatrer, friluftsmann
+                  og aktivist i norsk natur- og miljøvern. I Norge ble han
+                  særlig kjent gjennom sin deltakelse i aksjonene mot
+                  utbyggingen av Mardøla (1970) og Alta (1979–1981, se
+                  Alta-saken). Næss regnes som grunnleggeren av dypøkologien som
+                  filosofisk retning.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="beigeBar"></div>
+
+      <div className="container">
+        <div className="infoContainer">
+          <div className="arneHammerImg"></div>
+          <div className="box">
+            <div className="h2">Arkiv</div>
+            <div className="h3">
+              Proffessor, fjellklatrer, Ridder av St. Olavs Orden. Men hvem var
+              Arne Næss egentlig
+            </div>
+            <div className="h4">
+              Arne var en norsk filosof, kjent for sin lidenskap til natur og
+              mennesker. Han blir sett på som mannen bak dypøkologien og var en
+              veldig innflytningsrik filosof. Arne sine ideer om verden er like
+              aktuelle idag som de var når han levde.
+            </div>
+          </div>
+        </div>
+
+        <div className="infoContainer2">
+          <div className="box2">
+            <div className="h2">Arne Næss kunnskapssamling</div>
+            <div className="h4">
+              ANCEP har til nå identifisert i overkant av 3400 dokumenter. En
+              oversikt over disse finner du her. Samlingen inneholder alt fra;
+              bøker, artikler, brev, bilder.
+            </div>
+            <button className="btn">Kunnskapssamlingen</button>
+          </div>
+          <div className="arneAktivImg"></div>
+        </div>
+      </div>
+      <div className="beigeBar"></div>
+    </div>
+  );
+}
