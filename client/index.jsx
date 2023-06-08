2d2d8c693ca20c8671f07bce8cf3b13c166ce212
Oscar Våde <oscarvde@yahoo.com>
2023-06-08 16:45:12 +0200

    Endringer skkrt

--- client/index.jsx
+++ client/index.jsx
@@ -1,75 +1,94 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
+  import * as React from "react";
+  import { createRoot } from "react-dom/client";
+  import ReactToPrint from "react-to-print";
 
-import "./index.css";
-import "./diskusjon.css";
+  import "./index.css";
+  import "./diskusjon.css";
+  import "./diplom.css";
+  import "./kategori.css";
+  
 
-const resultatSide = (
-  <>
-  <div className="main-div">
-    <div className="containerHeader">
-				<div className="header">
-					<div className="logo"></div>
-					<h1 className="hjem">Hjem</h1>
-					<h1 className="dilemmaer">Dilemmaer</h1>
-					<h1 className="arkiv">Arkiv</h1>
-				</div>
-			</div>
-        {/* Tittel*/}
-    <div className="div-resultat-tittel">
-      <h1 className="tittel-resultat">Resultat</h1>   
-    </div>
-    {/* tekstbokser*/}
-    <div className="tekstBoks1">
-      <p className="boks1-p">Her har vi vurdert dine svar og sammenlignet med gjennomsnittet.
-      <p>
-      Du scoret høyest i krig, og lavest i dypøkologi. <p>
-       Vil du lære mer om disse? </p>
-      </p>
-      </p>
-      <button className="videreBtn"type="button">Ta meg videre</button>
-    </div>
-    <div className="tekstBoks2">
-      <p className="boks2-p">
-        Gratulerer med gjennomført spill! 
+  const resultatSide = (
+    <>
+    <div className="main-div">
+      <div className="containerHeader">
+          <div className="header">
+            <div className="logo"></div>
+            <h1 className="hjem">Hjem</h1>
+            <h1 className="dilemmaer">Dilemmaer</h1>
+            <h1 className="arkiv">Arkiv</h1>
+          </div>
+        </div>
+          {/* Tittel*/}
+      <div className="div-resultat-tittel">
+        <h1 className="tittel-resultat">Resultat</h1>   
+      </div>
+      {/* tekstbokser*/}
+      <div className="midt-div">
+        <div className="venstre-div">
+              {/* statistikk bokser*/}
+      <div className="dineSvar-div">
+        <div className="dineSvar-p-div">
+        <p className="dineSvar-p"> Dine svar </p>
+        </div>
+        <div className="blokker-ds-div">
+        <div className="dineSvarBlokk-1">
+          <p className="ds-score-blokk-1">10</p>
+          <p className="blokk-p-1">Krig</p>
+        </div>
+        <div className="dineSvarBlokk-2">
+        <p className="ds-score-blokk-2">8</p>
+        <p className="blokk-p-2">Dypøkologi</p>
+        </div>
+        <div className="dineSvarBlokk-3">
+        <p className="ds-score-blokk-3">15</p>
+        <p className="blokk-p-3">Minimalisme</p>
+        </div>
+        </div>
+        </div>
+      <div className="gjennomsnitt-div">
+        <div className="gjennomsnitt-p-div">
+        <p className="gjennomsnitt-p">Gjennomsnitt</p>
+        </div>
+        <div className="gs-blokker-div">
+      <div className="gjennomsnitt-blokk-1">
+          <p className="gs-score-blokk-1">10</p>
+          <p className="gs-blokk-p-1">Krig</p>
+        </div>
+        <div className="gjennomsnitt-blokk-2">
+        <p className="gs-score-blokk-2">10</p>
+        <p className="gs-blokk-p-2">Dypøkologi</p>
+        </div>
+        <div className="gjennomsnitt-blokk-3">
+        <p className="gs-score-blokk-3">15</p>
+        <p className="gs-blokk-p-3">Minimalisme</p>
+        </div>
+        </div>
+      </div>
+      </div>
+        <div className="høyre-div">
+        <div className="tekstBoks1">
+        <p className="boks-p">Her har vi vurdert dine svar og sammenlignet med gjennomsnittet.
         <p>
-          Om du vil kan du nå printe ut diplom.
+        Du scoret høyest i krig, og lavest i dypøkologi. <p>
+        Vil du lære mer om disse? </p>
         </p>
-      </p>
-      <button className="diplomBtn"type="button">Print diplom</button>
-    </div>
-     {/* statistikk bokser*/}
-    <div className="dineSvar-div">
-      <p className="dineSvar-p"> Dine svar </p>
-      <div className="dineSvarBlokk-1">
-        <p className="ds-score-blokk-1">10</p>
-        <p className="blokk-p-1">Krig</p>
+        </p>
+        <button className="btn"type="button">Ta meg videre</button>
       </div>
-      <div className="dineSvarBlokk-2">
-      <p className="ds-score-blokk-2">8</p>
-      <p className="blokk-p-2">Dypøkologi</p>
+      <div className="tekstBoks2">
+        <p className="boks-p">
+          Gratulerer med gjennomført spill! 
+          <p>
+            Om du vil kan du nå printe ut diplom.
+          </p>
+        </p>
+        <button className="btn"type="button" onClick={render}>Print diplom</button>
       </div>
-      <div className="dineSvarBlokk-3">
-      <p className="ds-score-blokk-3">15</p>
-      <p className="blokk-p-3">Minimalisme</p>
+        </div>
+        </div>
       </div>
-      </div>
-    <div className="gjennomsnitt-div">
-    <p className="gjennomsnitt-p">Gjennomsnitt</p>
-    <div className="gjennomsnitt-blokk-1">
-        <p className="gs-score-blokk-1">10</p>
-        <p className="gs-blokk-p-1">Krig</p>
-      </div>
-      <div className="gjennomsnitt-blokk-2">
-      <p className="gs-score-blokk-2">10</p>
-      <p className="gs-blokk-p-2">Dypøkologi</p>
-      </div>
-      <div className="gjennomsnitt-blokk-3">
-      <p className="gs-score-blokk-3">15</p>
-      <p className="gs-blokk-p-3">Minimalisme</p>
-      </div>
-    </div>
-    </div>
+
     </>
 );
 
@@ -78,7 +97,8 @@ const diskusjonSide = (
 <div className="main-div">
 <div className="containerHeader">
         <div className="header">
-          <div className="logo"></div>
+          <div className="logo">
+          </div>
           <h1 className="hjem">Hjem</h1>
           <h1 className="dilemmaer">dilemmaer</h1>
           <h1 className="arkiv">Arkiv</h1>
@@ -99,6 +119,83 @@ const diskusjonSide = (
   </>
 
 )
+
+const diplomSide = (
+  <>
+  <ReactToPrint
+  trigger={()=>{
+
+    return <button>Print this</button>
+  }}
+  
+  content = {()=>this.componentRef}
+  documentTitle='new document'
+  pageStyle="print"
+
+  
+  />
+    <div className="main-div">
+  <div ref={el=>(this.componentRef=el)} className="diplom-bilde">
+    hei
+  </div>
+<div className="containerHeader">
+    <div className="header">
+      <div className="logo"></div>
+      <h1 className="hjem">Hjem</h1>
+      <h1 className="dilemmaer">Dilemmaer</h1>
+      <h1 className="arkiv">Arkiv</h1>
+    </div>
+  </div>
+  <div className="print-diplom-div">
+    <button className="print-btn" type="button">Print ut!</button>
+  </div>
+  </div>
+  
+  </>
+)
+
+const kategoriSide = (
+  <>
+      <div className="page">
+      <div className="containerHeader">
+        <div className="header">
+          <div className="logo"></div>
+          <h1 className="hjem">Hjem</h1>
+          <h1 className="dilemmaer">Dilemmaer</h1>
+          <h1 className="arkiv">Arkiv</h1>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="heading">Velg kategori</div>
+        <div className="ageGroup">
+          
+          <div className="brownBox">
+            <div className="brownImg"></div>
+            <div className="text">Minimalisme <br /></div>
+          </div>
+
+          <div className="greenBox">
+            <div className="greenImg"></div>
+            <div className="text">Dypøkologi <br /></div>
+          </div>
+          <div className="purpBox">
+            <div className="purpImg"></div>
+            <div className="text">Krig <br /></div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </>
+)
+
+function render(){
+  root.render(diplomSide);
+}
 const element = document.getElementById("app");
 const root = createRoot(element);
 root.render(resultatSide);
+
+
