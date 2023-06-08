bf81026523ad12042cccf15bf0d5a9fcd541db8c
vytre <vegaytre@gmail.com>
2023-06-08 15:16:10 +0200

    Working on routing

--- client/application.jsx
+++ client/application.jsx
@@ -2,17 +2,18 @@ import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Frontpage } from "./pages/frontpage.jsx";
 import QuestionTest from "./utils/questiontest";
+import { DilemmaStart } from "./pages/dilemmaStart.jsx";
 
 export function Application() {
   return (
     <>
-
-        <Routes>
-          <Route path={"/"} element={<Frontpage />}></Route>
-          <Route path={"/arkiv"} element={<h1>Arkiv Side</h1>}></Route>
-          <Route path={"/dilemma"} element={<QuestionTest/>}></Route>
-          <Route path={"*"} element={<h1>Page not found</h1>}></Route>
-        </Routes>
+      <Routes>
+        <Route path={"/"} element={<Frontpage />}></Route>
+        <Route path={"/arkiv"} element={<h1>Arkiv Side</h1>}></Route>
+        {/*<Route path={"/dilemma"} element={<QuestionTest />}></Route>*/}
+        <Route path={"/dilemma"} element={<DilemmaStart />}></Route>
+        <Route path={"*"} element={<h1>Page not found</h1>}></Route>
+      </Routes>
     </>
   );
 }
---
+++ client/pages/dilemmaStart.jsx
@@ -0,0 +1,45 @@
+import { Header } from "../utils/header.jsx";
+import * as React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export function DilemmaStart() {
+  const questions = [
+    "Er det alltid galt å drepe? ",
+    "Bør man ofre personlig glede for naturen?",
+    "Er jeg dum?",
+  ];
+
+  const [index, setIndex] = useState(0);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/hvordan");
+  };
+
+  return (
+    <div className={"mainCon"}>
+      <Header />
+      <div className={"questContain"}>
+        <div className="introToGameContainer">
+          <div className="questionOneContainer">
+            <div className="questionsOneTextContainer">
+              <p className="questionOne">Velg Alder</p>
+            </div>
+            <div className="yesOrNo">
+              <div className="yes" onClick={handleClick}>
+                <p className="yesTextDesign">5-7</p>
+              </div>
+              <div className="no" onClick={handleClick}>
+                <p className="noTextDesign">8-10</p>
+              </div>
+              <div className="no" onClick={handleClick}>
+                <p className="noTextDesign">VGS</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
