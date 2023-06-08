10f9b40d8a35347560ec5b329e50a2215e0048cf
dpoxodp <89521915+dpoxodp@users.noreply.github.com>
2023-06-08 15:47:22 +0200

    wip soon done, ptr on way

--- client/pages/archive.jsx
+++ client/pages/archive.jsx
@@ -14,11 +14,19 @@ export function Archive() {
           </div>
           <div className="topBox">
               <div className="topInfoBox">
-                  <div className="heading">Arnes Historie</div>
-                  <div className="text">Arne Næss var en norsk filosof som var en de mest innflytelsesrike intellektuelle i Norge i andre halvdel av 1900-tallet. Gjennom Næss' innflytelse fikk analytisk filosofi en dominerende stilling i Norge i tiårene etter andre verdenskrig. Han var også kjent som fjellklatrer, friluftsmann og aktivist i norsk natur- og miljøvern. I Norge ble han særlig kjent gjennom sin deltakelse i aksjonene mot utbyggingen av Mardøla (1970) og Alta (1979–1981, se Alta-saken). Næss regnes som grunnleggeren av dypøkologien som filosofisk retning.</div>
+                  <div className="headingContainer">
+                      <div className="heading">Arnes Historie</div>
+                  </div>
+                  <div className="bgBoxContainer">
+                      <div className="text">Arne Næss var en norsk filosof som var en de mest innflytelsesrike intellektuelle i Norge i andre halvdel av 1900-tallet. Gjennom Næss' innflytelse fikk analytisk filosofi en dominerende stilling i Norge i tiårene etter andre verdenskrig. Han var også kjent som fjellklatrer, friluftsmann og aktivist i norsk natur- og miljøvern. I Norge ble han særlig kjent gjennom sin deltakelse i aksjonene mot utbyggingen av Mardøla (1970) og Alta (1979–1981, se Alta-saken). Næss regnes som grunnleggeren av dypøkologien som filosofisk retning.</div>
+                      <div className="bgBox"></div>
+                  </div>
               </div>
           </div>
       </div>
+
+      <div className="beigeBar"></div>
+
       <div className="container">
           <div className="infoContainer">
               <div className="arneHammerImg"></div>
@@ -30,14 +38,16 @@ export function Archive() {
           </div>
 
           <div className="infoContainer">
-              <div className="box">
-                  <div className="h2">Arne Næss kunnskapssamling</div>
+              <div className="box2">
+                  <div className="h2">Arne Næss kunnskapssamling
+                  </div>
                   <div className="h4">ANCEP har til nå identifisert i overkant av 3400 dokumenter. En oversikt over disse finner du her. Samlingen inneholder alt fra; bøker, artikler, brev, bilder.</div>
                   <button className="btn">Kunnskapssamlingen</button>
               </div>
               <div className="arneAktivImg"></div>
           </div>
       </div>
+      <div className="beigeBar"></div>
   </div>
     );
 }
