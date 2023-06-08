f3f0fce95e9e4e30ddfb167fb936fa82865074a3
vytre <vegaytre@gmail.com>
2023-06-08 11:38:00 +0200

    added "t" script, builds and runs in same script

--- client/application.jsx
+++ client/application.jsx
@@ -1,19 +1,18 @@
 import * as React from "react";
-import {Route, Routes} from "react-router-dom";
-import {Frontpage} from "./pages/frontpage.jsx";
-
+import { Route, Routes } from "react-router-dom";
+import { Frontpage } from "./pages/frontpage.jsx";
 
 export function Application() {
-    return (
-        <>
-            <main>
-                <Routes>
-                    <Route path={"/"} element={<Frontpage/>}></Route>
-                    <Route path={"/arkiv"} element={<h1>Arkiv Side</h1>}></Route>
-                    <Route path={"/dilemma"} element={<h1>Dilemma Side</h1>}></Route>
-                    <Route path={"*"} element={<h1>Page not found</h1>}></Route>
-                </Routes>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <Routes>
+          <Route path={"/"} element={<Frontpage />}></Route>
+          <Route path={"/arkiv"} element={<h1>Arkiv Side</h1>}></Route>
+          {/*<Route path={"/dilemma"} element={<IntroQuestionsOne />}></Route>*/}
+          <Route path={"*"} element={<h1>Page not found</h1>}></Route>
+        </Routes>
+      </main>
+    </>
+  );
 }
