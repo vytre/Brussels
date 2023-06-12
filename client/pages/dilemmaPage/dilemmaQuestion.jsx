da9bc3928b71a1bc3ebdd6855389b2231a36278c
vytre <vegaytre@gmail.com>
2023-06-12 15:37:16 +0200

    Finished yes no component

--- client/pages/dilemmaPage/dilemmaQuestion.jsx
+++ client/pages/dilemmaPage/dilemmaQuestion.jsx
@@ -4,48 +4,6 @@ import { useState } from "react";
 import { Header } from "../../utils/header.jsx";
 import { useNavigate, useParams } from "react-router-dom";
 
-/*const war = [
-  "Er det alltid feil å sende inn soldater i et annet land?",
-  "Har vi i Norge et ansvar for å hjelpe Ukraina i konflikten med Russland?\n(I dette spørsmålet ser vi bort ifra Nato medlemskap.)",
-  "Forestill deg at Norge er i en krigskonflikt\nHvis en krigsfange sitter på informasjon som kan redde 100 Norske soldater, er det greit å torturere krigsfangen som siste løsning for å redde de Norske soldatene?",
-];
-
-const deepEcology = [
-  "Er mennesker mer verdt enn dyr?",
-  "Er naturen mer verdt enn mennesker?",
-  "Er det verdt å teste på dyr?",
-];
-
-const minimalism = [
-  "Har alle et ansvar for å kjøpe færre ting i hverdagen?",
-  "Hvis man har mer enn det man trenger, har man en obligasjon om å gi bort ting til de som har mindre?",
-  "Er det riktig å bruke penger på ting som har symbolsk verdi?",
-];*/
-
-/*const questions = [
-  {
-    war: [
-      "Er det alltid feil å sende inn soldater i et annet land?",
-      "Har vi i Norge et ansvar for å hjelpe Ukraina i konflikten med Russland?\n(I dette spørsmålet ser vi bort ifra Nato medlemskap.)",
-      "Forestill deg at Norge er i en krigskonflikt\nHvis en krigsfange sitter på informasjon som kan redde 100 Norske soldater, er det greit å torturere krigsfangen som siste løsning for å redde de Norske soldatene?",
-    ],
-  },
-  {
-    deepEcology: [
-      "Er mennesker mer verdt enn dyr?",
-      "Er naturen mer verdt enn mennesker?",
-      "Er det verdt å teste på dyr?",
-    ],
-  },
-  {
-    minimalism: [
-      "Har alle et ansvar for å kjøpe færre ting i hverdagen?",
-      "Hvis man har mer enn det man trenger, har man en obligasjon om å gi bort ting til de som har mindre?",
-      "Er det riktig å bruke penger på ting som har symbolsk verdi?",
-    ],
-  },
-];*/
-
 const deepEcology = {
   1: {
     question: "Er mennesker mer verdt enn dyr?",
@@ -103,30 +61,26 @@ const war = {
   },
 };
 
-function getQuestion(category) {
-  let categoryQuestionObject = {};
+function getQuestion() {
+  const params = useParams();
+  const category = params.category;
 
+  let categoryQuestionObject = {};
   switch (category) {
     case "war":
       categoryQuestionObject = { ...war };
 
       console.log("war");
-      /*      console.log(war);
-      console.log(categoryQuestionObject[1].question);*/
       break;
     case "deepEcology":
       categoryQuestionObject = { ...deepEcology };
 
       console.log("deepEcology");
-      /*      console.log(deepEcology);
-      console.log(categoryQuestionObject[1].question);*/
       break;
     case "minimalism":
       categoryQuestionObject = { ...minimalism };
 
       console.log("minimalism");
-      /*      console.log(minimalism);
-      console.log(categoryQuestionObject[1].question);*/
       break;
     default:
       console.log("Bug....");
@@ -136,23 +90,29 @@ function getQuestion(category) {
 }
 
 export function DilemmaQuestion() {
-  const params = useParams();
-  const category = params.category;
-  const questionObject = getQuestion(category);
-
+  const questionObject = getQuestion();
   console.log(questionObject);
 
   const [index, setIndex] = useState(1);
+  const [question, setQuestion] = useState(questionObject[1].question);
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleClick = (value) => {
     if (index < 4) {
-      setIndex(index + 1);
+      if (question === questionObject[index].question) {
+        if (value === "yes") {
+          setQuestion(questionObject[index].yes);
+        } else if (value === "no") {
+          setQuestion(questionObject[index].no);
+        }
+        setIndex(index + 1);
+      } else if (question !== questionObject[index].question) {
+        setQuestion(questionObject[index].question);
+      }
     }
-    if (index >= 4 - 1) {
+    if (index >= 4) {
       navigate("/dilemma/crossroads");
     }
-    console.log(index);
   };
 
   return (
@@ -161,13 +121,13 @@ export function DilemmaQuestion() {
       <div className="introToGameContainer43">
         <div className="questionOneContainer43">
           <div className="questionsOneTextContainer43">
-            <p className="questionOne43">{questionObject[index].question}</p>
+            <p className="questionOne43">{question}</p>
           </div>
           <div className="yesOrNo43">
-            <div className="yes43" onClick={handleClick}>
+            <div className="yes43" onClick={() => handleClick("yes")}>
               <p className="yesTextDesign43">Ja</p>
             </div>
-            <div className="no43" onClick={handleClick}>
+            <div className="no43" onClick={() => handleClick("no")}>
               <p className="noTextDesign43">Nei</p>
             </div>
           </div>
