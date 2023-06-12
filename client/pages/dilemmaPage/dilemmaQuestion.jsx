02248f73f2f9c810b1470576e81b97f4bd23bcbc
vytre <vegaytre@gmail.com>
2023-06-12 13:30:35 +0200

    Converted questions from lists to javascript objects

--- client/pages/dilemmaPage/dilemmaQuestion.jsx
+++ client/pages/dilemmaPage/dilemmaQuestion.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Header } from "../../utils/header.jsx";
 import { useNavigate, useParams } from "react-router-dom";
 
-const war = [
+/*const war = [
   "Er det alltid feil å sende inn soldater i et annet land?",
   "Har vi i Norge et ansvar for å hjelpe Ukraina i konflikten med Russland?\n(I dette spørsmålet ser vi bort ifra Nato medlemskap.)",
   "Forestill deg at Norge er i en krigskonflikt\nHvis en krigsfange sitter på informasjon som kan redde 100 Norske soldater, er det greit å torturere krigsfangen som siste løsning for å redde de Norske soldatene?",
@@ -20,7 +20,7 @@ const minimalism = [
   "Har alle et ansvar for å kjøpe færre ting i hverdagen?",
   "Hvis man har mer enn det man trenger, har man en obligasjon om å gi bort ting til de som har mindre?",
   "Er det riktig å bruke penger på ting som har symbolsk verdi?",
-];
+];*/
 
 /*const questions = [
   {
@@ -46,50 +46,113 @@ const minimalism = [
   },
 ];*/
 
+const deepEcology = {
+  1: {
+    question: "Er mennesker mer verdt enn dyr?",
+    yes: "Er kjældyr mer verdt enn et mennesker du ikke kjenner?",
+    no: "Ville du reddet en panda framfor en person?",
+  },
+  2: {
+    question: "Er naturen mer verdt enn mennesker?",
+    yes: "Er verdi basert på hva man kan skape?",
+    no: "Uten naturen kan ikke mennesker overleve. Er menneske fortsatt mer verdt?",
+  },
+  3: {
+    question: "Er det verdt å teste på dyr?",
+    yes: "Hva om det kun er for kosmetiske grunner?",
+    no: "Hva om det kan redde menneskeliv?",
+  },
+};
+
+const minimalism = {
+  1: {
+    question: "Har alle et ansvar for å kjøpe færre ting i hverdagen?",
+    yes: "Hva om dette betyr at folk mister jobben sin, er det fortsatt riktig?",
+    no: "Hva om dette går utover menneskeliv og miljø?",
+  },
+  2: {
+    question:
+      "Hvis man har mer enn det man trenger, har man en obligasjon om å gi bort ting til de som har mindre?",
+    yes: "Hva om livskvaliteten blir redusert for den som må gi bort?",
+    no: "Hva om man har mulighet til å redde liv uten å redusere sin egen livskvalitet?",
+  },
+  3: {
+    question: "Er det riktig å bruke penger på ting som har symbolsk verdi?",
+    yes: "Ved å ikke kjøpe en dyr klokke så kan man redde 100 menneskeliv, er det fortsatt greit å kjøpe klokken?",
+    no: "Hva om man får glede av en gjenstand, er det fortsatt ikke greit å kjøpe en klokke for eksempel? ",
+  },
+};
+
+const war = {
+  1: {
+    question: "Er det alltid feil aa sende inn soldater i et annet land?",
+    yes: "Hva om det er brudd på menneskerettigheter, er det fortsatt alltid feil?",
+    no: "Hva om det eskalerer en situasjon, er det fortsatt riktig?",
+  },
+  2: {
+    question:
+      "Har vi i Norge et ansvar for å hjelpe Ukraina i konflikten med Russland? ( I dette spørsmålet ser vi bort ifra Nato medlemskap.)",
+    yes: "Betyr det at vi har samme ansvar for hvilket som helst land?",
+    no: "Hva om Russland hadde truet Norges frihet, betyr det at andre land ikke burde ha hjulpet Norge?",
+  },
+  3: {
+    question:
+      "Forestill deg at Norge er i en krigskonflikt Hvis en krigsfange sitter på informasjon som kan redde 100 Norske soldater, er det greit torturere krigsfangen som siste løsning for å redde de Norske soldatene?",
+    yes: "Hva om det er brudd på menneskerettigheter, er det fortsatt alltid feil?",
+    no: "Hva om det eskalerer en situasjon, er det fortsatt riktig?",
+  },
+};
+
 function getQuestion(category) {
-  let categoryQuestionList = [];
+  let categoryQuestionObject = {};
 
   switch (category) {
     case "war":
-      categoryQuestionList = [...war];
+      categoryQuestionObject = { ...war };
 
       console.log("war");
-      console.log(categoryQuestionList[0]);
+      /*      console.log(war);
+      console.log(categoryQuestionObject[1].question);*/
       break;
     case "deepEcology":
-      categoryQuestionList = [...deepEcology];
+      categoryQuestionObject = { ...deepEcology };
 
       console.log("deepEcology");
-      console.log(categoryQuestionList);
+      /*      console.log(deepEcology);
+      console.log(categoryQuestionObject[1].question);*/
       break;
     case "minimalism":
-      categoryQuestionList = [...minimalism];
+      categoryQuestionObject = { ...minimalism };
 
       console.log("minimalism");
-      console.log(categoryQuestionList[0]);
+      /*      console.log(minimalism);
+      console.log(categoryQuestionObject[1].question);*/
       break;
     default:
       console.log("Bug....");
       break;
   }
-  return categoryQuestionList;
+  return categoryQuestionObject;
 }
 
 export function DilemmaQuestion() {
   const params = useParams();
   const category = params.category;
-  const questions = getQuestion(category);
+  const questionObject = getQuestion(category);
 
-  const [index, setIndex] = useState(0);
+  console.log(questionObject);
+
+  const [index, setIndex] = useState(1);
   const navigate = useNavigate();
 
   const handleClick = () => {
-    if (index < questions.length) {
+    if (index < 4) {
       setIndex(index + 1);
     }
-    if (index >= questions.length - 1) {
+    if (index >= 4 - 1) {
       navigate("/dilemma/crossroads");
     }
+    console.log(index);
   };
 
   return (
@@ -98,7 +161,7 @@ export function DilemmaQuestion() {
       <div className="introToGameContainer43">
         <div className="questionOneContainer43">
           <div className="questionsOneTextContainer43">
-            <p className="questionOne43">{questions[index]}</p>
+            <p className="questionOne43">{questionObject[index].question}</p>
           </div>
           <div className="yesOrNo43">
             <div className="yes43" onClick={handleClick}>
