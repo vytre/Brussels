5473e11cf247c427d7768d2c1d026d62d1f20272
dpoxodp <89521915+dpoxodp@users.noreply.github.com>
2023-06-13 14:41:38 +0200

    Finished refactoring/merging for crossroad

--- client/pages/dilemma/crossroad.jsx
+++ client/pages/dilemma/crossroad.jsx
@@ -1,52 +1,78 @@
 import "../../stylesheets/dilemma/crossroad.css";
 import { Header } from "../../utils/header.jsx";
-import {useNavigate, useParams} from "react-router-dom";
-import {useState} from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
 
 const deepEcology = {
   1: {
-    crossroad: "Burde man innføre kjøttfrie dager i kantinen på skolen? Trykk på salaten dersom du mener dette burde innføres, eller biffen dersom du mener dette ikke burde innføres.",
+    crossroad:
+      "Burde man innføre kjøttfrie dager i kantinen på skolen? Trykk på salaten dersom du mener dette burde innføres, eller biffen dersom du mener dette ikke burde innføres.",
     answer1: "Ja",
-    answer2: "Nei"},
+    answer2: "Nei",
+  },
   2: {
-    crossroad: "Burde vi slutte med oljeutvinning, og kun fokusere på fornybar energi? Hvis ja, trykk på vindmøllen. Hvis nei, trykk på oljeriggen.",
+    crossroad:
+      "Burde vi slutte med oljeutvinning, og kun fokusere på fornybar energi? Hvis ja, trykk på vindmøllen. Hvis nei, trykk på oljeriggen.",
     answer1: "Ja",
-    answer2: "Nei"},
+    answer2: "Nei",
+  },
   3: {
-    crossroad: "Har forbrukere større ansvar for resirkulering enn selskaper som lager produkter? Hvis ja, trykk på søppelbøtten. Hvis nei, trykk på mannen med søppel bak seg.",
+    crossroad:
+      "Har forbrukere større ansvar for resirkulering enn selskaper som lager produkter? Hvis ja, trykk på søppelbøtten. Hvis nei, trykk på mannen med søppel bak seg.",
     answer1: "Ja",
-    answer2: "Nei"
+    answer2: "Nei",
   },
 };
 
 const minimalism = {
   1: {
-    crossroad: "I the crossroad",
-    answer1: "answer1Test",
-    answer2: "answer2Test"},
+    crossroad:
+      "La oss si at det er 1 år siden du kjøpte en ny telefon. \n" +
+      "Burde man la være å kjøpe en ny telefon etter 1 år? \n" +
+      "Hvis ja, trykk på den gamle telefon. Hvis nei, trykk på de nyere telefonene",
+    answer1: "Ja",
+    answer2: "Nei",
+  },
   2: {
-    crossroad: "I the crossroad",
-    answer1: "answer1Test",
-    answer2: "answer2Test"},
+    crossroad:
+      'I enkelte U-land er det mennesker som jobber under dårlige kår, dette blir ofte bli omtalt som "sweatshops", \n' +
+      "og i mange tilfeller har disse fabrikkene høyt utslipp av Co2. Er det riktig å ikke kjøpe varer fra U-land,\n" +
+      "og med dette risikere inntekten til de som jobber der, eller burde man alltid tenke på miljøet først?\n" +
+      "Trykk på treet hvis du mener at naturen alltid kommer først. Trykk arbeiderne på hvis du mener at man burde fortsette\n" +
+      "å kjøpe varer med høyt Co2-utslipp, og sørge for at arbeiderne fortsatt har en jobb.",
+    answer1: "Naturen først",
+    answer2: "Mennesket først",
+  },
   3: {
-    crossroad: "I the crossroad",
-    answer1: "answer1Test",
-    answer2: "answer2Test"
+    crossroad:
+      "Har vi alle et ansvar for å kjøpe mer brukte klær for å redusere overforbruk? Hvis ja, trykk på den grønne posen. Hvis nei, trykk på den røde posen.",
+    answer1: "Ja",
+    answer2: "Nei",
   },
 };
 const war = {
   1: {
-    crossroad: "I the crossroad",
-    answer1: "answer1Test",
-    answer2: "answer2Test"},
+    crossroad:
+      "Se for deg at du er i en krigssone, og du er på vei til et viktig oppdrag. Du møter en gjeter, og du vet ikke om gjeteren er til å stole på.\n" +
+      "Lar du gjeter gå, og riskerer at han kan oppgi posisjonen din til fienden? Eller holder du gjeteren fanget?\n" +
+      "Trykk på bildet hvor gjeteren går hvis du mener at han burde få gå. Trykk på bildet hvor gjeteren er fanget hvis du mener han burde være fanget.",
+    answer1: "Fri",
+    answer2: "Fanget",
+  },
   2: {
-    crossroad: "I the crossroad",
-    answer1: "answer1Test",
-    answer2: "answer2Test"},
+    crossroad:
+      "Se for deg samme dilemma som forrige, men i dette tilfellet så er det garantert at gjeteren kommer til å sladre, og du kan ikke ta med deg eller holde gjeteren fanget.\n" +
+      "Er det da riktig å drepe gjeteren, eller å la gjeteren gå? Trykk på bilde hvor gjeteren går hvis du mener han burde få gå. Trykk på bilde hvor gjeteren\n" +
+      "er fanget hvis du mener det er riktig å drepe.",
+    answer1: "Drepe",
+    answer2: "Slipp fri",
+  },
   3: {
-    crossroad: "I the crossroad",
-    answer1: "answer1Test",
-    answer2: "answer2Test"
+    crossroad:
+      "Er det riktig at Norske fremmedkrigere reiser til Ukraina for å kjempe for Ukraina? \n" +
+      "Hvis ja, trykk på det Ukrainske flagget. Hvis nei, trykk på det Norske flagget.",
+    answer1: "Ja",
+    answer2: "Nei",
   },
 };
 export function Crossroad() {
@@ -54,9 +80,9 @@ export function Crossroad() {
   const category = params.category;
   const ageGroup = params.ageGroup;
   const workMode = params.workMode;
-  function getCrossroad(){
+  function getCrossroad() {
     let categoryCrossroadObject = {};
-    switch (category){
+    switch (category) {
       case "war":
         categoryCrossroadObject = { ...war };
         console.log("war");
@@ -79,22 +105,27 @@ export function Crossroad() {
   const navigate = useNavigate();
   const categoryCrossroadObject = getCrossroad();
   const [index, setIndex] = useState(1);
-  const [crossroad, setCrossroad] = useState(categoryCrossroadObject[1].crossroad);
+  const [crossroad, setCrossroad] = useState(
+    categoryCrossroadObject[1].crossroad
+  );
 
   const handleClick = (value) => {
-
     //hardcoded value with a getLength of the json obj?
-   if(index < 4){
-     if(value === 1){
-       //STORE USER ANSWER HERE with agegroup, category, crossroad index
-     } else if(value === 2){
-       //STORE USER ANSWER HERE with agegroup, category, crossroad index
-     }
-     setIndex(index + 1);
-     setCrossroad(categoryCrossroadObject[index].crossroad)
-   }else if (index >= 4) {
-     navigate(`/dilemma/${ageGroup}/${workMode}/${category}/result`);
-   }
+
+    console.log(index);
+    setIndex(index + 1);
+    setCrossroad(categoryCrossroadObject[index].crossroad);
+
+    if (index < 4) {
+      console.log(index);
+      if (value === 1) {
+        //STORE USER ANSWER HERE with agegroup, category, crossroad index
+      } else if (value === 2) {
+        //STORE USER ANSWER HERE with agegroup, category, crossroad index
+      }
+    } else if (index >= 4) {
+      navigate(`/dilemma/${ageGroup}/${workMode}/${category}/result`);
+    }
   };
 
   return (
@@ -106,16 +137,18 @@ export function Crossroad() {
         </div>
 
         <div className="dilemmaTextContainer">
-          <p className="dilemmaText">
-            Burde man innføre kjøttfrie dager i kantinen på skolen? Trykk på
-            salaten dersom du mener dette burde innføres, eller biffen dersom du
-            mener dette ikke burde innføres.
-          </p>
+          <p className="dilemmaText">{crossroad}</p>
         </div>
 
         <div className="dilemmaImageContainer">
-          <div className={(category) + "Crossroad" + (index) + "Answer1"} onClick={()=> handleClick(1)} />
-          <div className={(category) + "Crossroad" + (index) + "Answer2"} onClick={()=> handleClick(2)} />
+          <div
+            className={category + "Crossroad" + index + "Answer1"}
+            onClick={() => handleClick(1)}
+          />
+          <div
+            className={category + "Crossroad" + index + "Answer2"}
+            onClick={() => handleClick(2)}
+          />
         </div>
       </div>
     </div>
